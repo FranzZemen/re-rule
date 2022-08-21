ca040fd20bac4aca7864c890550c0490e727af4b
Franz Zemen <franzzemen@gmail.com>
2022-08-21 09:19:07 -0400

    constant

--- ts-src/util/rule-hint-key.ts
+++ ts-src/util/rule-hint-key.ts
@@ -1,7 +1,7 @@
 import {LogicalConditionHintKey} from '@franzzemen/re-logical-condition';
 
 export class RuleHintKey extends LogicalConditionHintKey {
-  public static RulesEngine = 'RulesEngine';
+  public static RulesEngine = 're';
   public static Application = 'ap';
   public static RuleSet = 'rs';
   public static Rule = 'ru';
