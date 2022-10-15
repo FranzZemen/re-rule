d4e9061b49ac3538c2bc538b9193550e693add6f
Franz Zemen <franzzemen@gmail.com>
2022-10-15 14:26:54 -0400

    Added rule parser to scope

--- ts-src/scope/rule-scope.ts
+++ ts-src/scope/rule-scope.ts
@@ -1,11 +1,14 @@
-import {ExecutionContextI, Hints} from '@franzzemen/app-utility';
+import {ExecutionContextI} from '@franzzemen/app-utility';
 import {Scope} from '@franzzemen/re-common';
 import {LogicalConditionScope} from '@franzzemen/re-logical-condition';
 import {RuleOptions} from './rule-options.js';
+import {RuleParser} from '../parser/rule-parser.js';
 
 export class RuleScope extends LogicalConditionScope {
   public static ParentScope = 'ParentScope';
+  public static RuleParser = 'RuleParser';
   constructor(options?: RuleOptions, parentScope?: Scope, ec?: ExecutionContextI) {
     super(options, parentScope, ec);
+    this.set(RuleScope.RuleParser, new RuleParser());
   }
 }
